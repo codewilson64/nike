9ae1ffecc293cadc2a3aa9ce97487072dfeb8bc0
codewilson64 <wilsongambit@gmail.com>
2025-10-20 15:21:22 +0700

    feature/midtrans

--- src/lib/@types/midtrans.d.ts
+++ src/lib/@types/midtrans.d.ts
@@ -1,18 +1,28 @@
 declare global {
-    interface Window {
-      snap: {
-        pay: (
-          token: string,
-          options?: {
-            onSuccess?: (result: any) => void;
-            onPending?: (result: any) => void;
-            onError?: (result: any) => void;
-            onClose?: () => void;
-          }
-        ) => void;
-      };
-    }
+  interface MidtransResult {
+    transaction_id: string;
+    order_id: string;
+    gross_amount: string;
+    payment_type: string;
+    transaction_status: string;
+    fraud_status?: string;
   }
-  
-  export {};
+
+  interface Window {
+    snap: {
+      pay: (
+        token: string,
+        options?: {
+          onSuccess?: (result: MidtransResult) => void;
+          onPending?: (result: MidtransResult) => void;
+          onError?: (result: MidtransResult) => void;
+          onClose?: () => void;
+        }
+      ) => void;
+    };
+  }
+}
+
+export {};
+
   
